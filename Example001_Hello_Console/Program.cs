e67a42e9783b519d1948b6a836ef9682c5cd7f02
Tatyana Emelyanova <beatriss208@gmail.com>
2022-11-18 14:58:41 +0300

    new

--- Example001_Hello_Console/Program.cs
+++ Example001_Hello_Console/Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+string username = Console.WriteLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
