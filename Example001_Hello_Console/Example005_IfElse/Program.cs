d05f420748dcf5563ee1b098bb023b9287a7ba62
Tatyana Emelyanova <beatriss208@gmail.com>
2022-11-18 16:06:59 +0300

    dr

---
+++ Example001_Hello_Console/Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "таня")
+{
+    Console.WriteLine("Ура это же Таня!");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
